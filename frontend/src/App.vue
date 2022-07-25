85b2b2e8be09b471d6811b0e845d620235b15819
rlagnswo0505 <rlagnswo0505@naver.com>
2022-07-25 23:36:55 +0900

    설치를 위한 삭제

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,30 @@
 <template>
-  <div>
-    <Header />
-    <router-view />
-    <Footer />
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import Header from './layout/Header.vue';
-import Footer from './layout/Footer.vue';
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  components: { Header, Footer },
-  computed: {},
-};
-</script>
+nav {
+  padding: 30px;
+}
 
-<style></style>
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
