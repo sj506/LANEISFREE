cfc60c234cd83bc7b9133cee238939a1106d2ae1
HoonJae <rlagnswo0505@naver.com>
2022-07-25 17:08:14 +0900

    index.js 업데이트

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -28,27 +28,9 @@ const routes = [
   },
   {
     path: '/detail',
-    name: 'hommeDetail',    
-    component: hommeDetail
-  }
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
-  {
-    path: '/detail',
-    name: 'hommeDetail',    
-    component: hommeDetail
-  }
+    name: 'hommeDetail',
+    component: hommeDetail,
+  },
 ];
 
 const router = createRouter({
