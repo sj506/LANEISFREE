d57708e36e9df84df9c6910cd2738ff07e67b8f3
Seung-jae <tmdwo5040@gmail.com>
2022-07-25 15:29:47 +0900

    css 버그 수정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import mixins from './mixins';
+import css from './assets/css/style.css';
 
-createApp(App).use(store).use(router).mixin(mixins).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).mixin(mixins).use(store).use(router).mount('#app').use(css);
