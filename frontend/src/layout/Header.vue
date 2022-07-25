4086636bf78984465e4f42239f503be6631b4756
rlagnswo0505 <rlagnswo0505@naver.com>
2022-07-26 00:46:53 +0900

    header toggle bar

--- frontend/src/layout/Header.vue
+++ frontend/src/layout/Header.vue
@@ -1,28 +1,32 @@
 <template>
-  <div class="column-center">
-    <div>
-      <div class="logo">
+  <header>
+    <nav class="navbar">
+      <div class="navbar__logo">
         <img class="logo-img" src="https://www.laneige.com/kr/ko/assets/image/a/laneige-logo.svg" alt="" />
       </div>
-      <div>
-        <nav>
-          <ul>
-            <li>브랜드</li>
-            <li>소개</li>
-            <li>미츠 아트</li>
-            <li>베스트</li>
-            <li>신상품</li>
-            <li>옴므</li>
-          </ul>
-        </nav>
-      </div>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
-  </div>
+      <ul class="navbar__menu" :class="{ active: isActive }">
+        <li>브랜드</li>
+        <li>미츠아트</li>
+        <li>베스트</li>
+        <li>신상품</li>
+        <li>옴므</li>
+      </ul>
+      <ul class="navbar__icons" :class="{ active: isActive }">
+        <li>
+          <i class="fa-solid fa-user"></i>
+        </li>
+        <li>
+          <i class="fa-solid fa-location-dot"></i>
+        </li>
+        <li>
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </li>
+      </ul>
+      <a href="#" class="navbar__toogleBtn" @click="toggleNav">
+        <i class="fa-solid fa-bars"></i>
+      </a>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -31,7 +35,7 @@ export default {
   components: {},
   data() {
     return {
-      example: '',
+      isActive: false,
     };
   },
   beforeCreate() {},
@@ -42,7 +46,11 @@ export default {
   updated() {},
   beforeUnmount() {},
   unmounted() {},
-  methods: {},
+  methods: {
+    toggleNav() {
+      this.isActive = !this.isActive;
+    },
+  },
 };
 </script>
 
@@ -55,9 +63,78 @@ export default {
   margin-left: 50px;
   margin-right: 100px;
 }
-.logo {
+.navbar__logo {
   display: flex;
   flex-direction: row;
   margin: 20px;
+}
+ul {
+  padding-left: 0;
+  list-style: none;
+}
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 12px;
+  align-items: center;
+  color: var(--text-light-gray);
+}
+.navbar__menu {
+  display: flex;
+  justify-content: center;
+}
+.navbar__menu li {
+  padding: 8px 12px;
+}
+.navbar__menu li:hover {
+  background-color: var(--main-color);
+  border-radius: 4px;
+}
+.navbar__icons {
+  display: flex;
+}
+.navbar__icons li {
+  padding: 8px 12px;
+}
+.navbar__icons i:hover {
+  color: var(--text-point);
+}
+.navbar__toogleBtn {
+  position: absolute;
+  display: none;
+  right: 10px;
+  font-size: 24px;
+  color: var(--text-main);
+}
+@media screen and (max-width: 768px) {
+  .navbar {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .navbar__menu {
+    display: none;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+    width: 100%;
+    padding: 8px 12px;
+    box-sizing: border-box;
+  }
+  .navbar__menu li {
+    width: 100%;
+  }
+  .navbar__icons {
+    display: none;
+    justify-content: center;
+    width: 100%;
+    text-align: center;
+  }
+  .navbar__toogleBtn {
+    display: block;
+  }
+  .navbar__menu.active,
+  .navbar__icons.active {
+    display: flex;
+  }
 }
 </style>
