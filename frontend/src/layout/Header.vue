13280f9a020116f8a6f62d36c65b4feacf04bbe8
HoonJae <rlagnswo0505@naver.com>
2022-07-25 16:24:10 +0900

    되돌리기

---
+++ frontend/src/layout/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="column-center">
+    <div>
+      <img class="logo" src="https://www.laneige.com/kr/ko/assets/image/a/laneige-logo.svg" alt="" />
+    </div>
+    <nav>
+      <!-- 자기 라우터여기에 하나씩 추가하면 됨 -->
+    </nav>
+  </div>
+  <hr />
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  data() {
+    return {
+      example: '',
+    };
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+
+<style>
+.logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 130px;
+}
+</style>
