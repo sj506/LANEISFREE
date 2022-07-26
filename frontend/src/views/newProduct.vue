537903bc5f4c1bc6fd8f09605f90f09a84642618
Git-DH <kdh9345@naver.com>
2022-07-26 16:18:48 +0900

    ..

--- frontend/src/views/newProduct.vue
+++ frontend/src/views/newProduct.vue
@@ -1,17 +1,46 @@
 <template>
-  <main>
-    <div>
-        
+  <main id="main" class="main">
+    <ol>
+      <li>
+        <router-link to="/index">
+          <!-- 링크주소 넣기-->
+          <span>Main</span>
+        </router-link>
+      </li>
+      <li class="liList">
+        >
+        <router-link to="/new">
+          <!-- 링크주소 넣기-->
+          <span>신상품</span>
+        </router-link>
+      </li>
+    </ol>
+    <div class="mainTitle">
+      <header>
+        <h2>신상품</h2>
+      </header>
+    </div>
+    
+    <div class="container">
+      <div>
+        <img src="../newImage/9.jpg" alt="">
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   </main>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+h2 {
+  text-align: center;
+}
+li {
+  display: inline;
+}
 </style>
