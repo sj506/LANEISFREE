b15305b42d455aebfbed28a2f53e9d83684381e0
HoonJae <rlagnswo0505@naver.com>
2022-07-27 11:42:34 +0900

    스와이프 작업 중

--- frontend/src/components/ListItem.vue
+++ frontend/src/components/ListItem.vue
@@ -1,42 +1,71 @@
 <template>
-  <swiper :modules="modules" :slides-per-view="3" :space-between="50" navigation :pagination="{ clickable: true }" :scrollbar="{ draggable: true }" @swiper="onSwiper" @slideChange="onSlideChange">
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    ...
+  <swiper
+    :slidesPerView="1"
+    :spaceBetween="30"
+    :centeredSlides="true"
+    :loop="true"
+    :autoplay="{
+      delay: 3000,
+      disableOnInteraction: false,
+    }"
+    :pagination="{
+      clickable: true,
+    }"
+    :navigation="true"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide><img src="https://www.laneige.com/kr/ko/layout/main/key-visual/__icsFiles/afieldfile/2022/06/07/20220607_final_HUG_main_KV_pc.jpg" alt="" /></swiper-slide>
+    <swiper-slide><img src="https://www.laneige.com/kr/ko/layout/main/key-visual/__icsFiles/afieldfile/2022/02/22/20220222_final_WATER-BANK-BLUE-HYALURONIC_main_KV_pc.png" alt="" /></swiper-slide>
+    <swiper-slide><img src="https://www.laneige.com/kr/ko/layout/main/key-visual/__icsFiles/afieldfile/2022/05/19/220523_final_NEO-NOVO_main_kv_pc.jpg" alt="" /></swiper-slide>
+    <swiper-slide><img src="https://www.laneige.com/kr/ko/layout/main/key-visual/__icsFiles/afieldfile/2022/05/19/20220516_final_WATER-BANK-BLUE-HYALURONIC-CLEANSING_main_KV_pc.jpg" alt="" /></swiper-slide>
+    <swiper-slide><img src="https://www.laneige.com/kr/ko/layout/main/key-visual/__icsFiles/afieldfile/2022/04/28/20220428_final_BESPOKE-NEO_main_KV_pc.jpg" alt="" /></swiper-slide>
   </swiper>
 </template>
 <script>
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
-// Import Swiper styles
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper';
+
 export default {
   components: {
     Swiper,
     SwiperSlide,
   },
   setup() {
-    const onSwiper = (swiper) => {
-      console.log(swiper);
-    };
-    const onSlideChange = () => {
-      console.log('slide change');
-    };
     return {
-      onSwiper,
-      onSlideChange,
-      modules: [Navigation, Pagination, Scrollbar, A11y],
+      modules: [Autoplay, Pagination, Navigation],
     };
   },
 };
 </script>
+
+<style scoped>
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
--- frontend/src/views/index.vue
+++ frontend/src/views/index.vue
@@ -19,4 +19,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin-top: 60px;
+}
+</style>
