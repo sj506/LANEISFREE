c7b8abbd8a43391407bbd673368f0e43abc785e2
rlagnswo0505 <rlagnswo0505@naver.com>
2022-07-29 23:07:07 +0900

    scroll snap text

--- frontend/src/views/story.vue
+++ frontend/src/views/story.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>story</h1>
+    <section id="item-1" class="item">첫번째 섹션</section>
+    <section id="item-2" class="item">두번째 섹션</section>
+    <section id="item-3" class="item">세번째 섹션</section>
   </div>
 </template>
 
@@ -20,4 +22,32 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+html,
+body {
+  width: 100vw;
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+}
+
+html {
+}
+
+.item {
+  width: 100vw;
+  height: 100vh;
+
+  scroll-snap-align: center;
+  scroll-snap-stop: always;
+}
+
+.item:nth-child(1) {
+  background: green;
+}
+.item:nth-child(2) {
+  background: blue;
+}
+.item:nth-child(3) {
+  background: yellow;
+}
+</style>
