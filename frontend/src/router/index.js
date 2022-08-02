30e4ff80d22103a90d96769611fde58af31383fc
Seung-jae <tmdwo5040@gmail.com>
2022-08-02 17:00:57 +0900

    상세페이지 완료

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,6 +9,7 @@ import signIn from '../views/user/signIn.vue';
 import signUp from '../views/user/signUp.vue';
 import story from '../views/story.vue';
 import meetsArt from '../views/meetsArt.vue';
+import hommeProductDetail from '../views/homme/hommeProductDetail.vue';
 
 const routes = [
   {
@@ -60,6 +61,12 @@ const routes = [
     path: '/meetsart',
     name: 'meetsart',
     component: meetsArt,
+  },
+  {
+    path: '/hommeProductDetail/:productId',
+    name: 'hommeProductDetail',
+    component: hommeProductDetail,
+    props: true,
   },
   // {
   //   path: '/',
