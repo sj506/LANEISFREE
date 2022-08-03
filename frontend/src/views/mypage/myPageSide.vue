c20c26a1bbbdfcc2c52b039eb1dbb6acdf0744db
CHAN YE <qjdrptmzk@naver.com>
2022-08-03 11:41:29 +0900

    병합커밋

---
+++ frontend/src/views/mypage/myPageSide.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav id="snb">
+		<div class="inner">
+			<ul class="navList">
+				<li>
+                    <a href="" onclick="">주문/배송현황</a>
+                </li>
+				<li><a href="">찜한 제품</a></li>
+				<li><a href="">내 리뷰</a></li>
+				<li class="active"><a href="">회원정보 수정</a></li>
+			</ul>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
