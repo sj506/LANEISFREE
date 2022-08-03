fef4c3a9f1161feaa5ce53686fa07ca5b8b9cfdc
Git-DH <kdh9345@naver.com>
2022-08-03 11:42:04 +0900

    옴므 안나오는거 수정

--- frontend/src/views/bestProduct.vue
+++ frontend/src/views/bestProduct.vue
@@ -4,23 +4,20 @@
       <h2 class="best_seller">BEST SELLER</h2>
     </div>
     <div class="container">
-      <div class="container_1">
-        <div class="flexWrap_1">
+        <!-- <div class="flexWrap_1"> -->
           <div class="flexbox">
             <img
               src="../assets/img/bsetProduct/크림스킨가로.jpg"
               alt="크림스킨"
             />
           </div>
-        </div>
+        <!-- </div> -->
         <div class="flexWrap1">
           <div class="flexbox_1">
             <img
               src="../assets/img/bsetProduct/네오쿠션가로.jpg"
               alt="네오쿠션"
             />
-          </div>
-          <div class="flexbox_1">
             <img
               src="../assets/img/bsetProduct/네오파운데이션매트가로.jpg"
               alt="네오파운데이션매트"
@@ -33,8 +30,6 @@
               src="../assets/img/bsetProduct/래디언C크림가로.jpg"
               alt="래디언C크림"
             />
-          </div>
-          <div class="flexbox_1">
             <img
               src="../assets/img/bsetProduct/립슬리핑마스크EX가로.jpg"
               alt="립슬핑마스크EX"
@@ -47,8 +42,6 @@
               src="../assets/img/bsetProduct/워터슬리핑마스크EX가로.jpg"
               alt="워터슬리핑마스크EX"
             />
-          </div>
-          <div class="flexbox_1">
             <img
               src="../assets/img/bsetProduct/워터뱅크블루히알루로닉크림가로.jpg"
               alt="워터뱅크블루히알루로닉크림"
@@ -56,7 +49,6 @@
           </div>
         </div>
       </div>
-    </div>
   </main>
 </template>
 
@@ -72,7 +64,7 @@ main {
 .best_title {
   text-align: center;
 }
-.container_1 {
+.container {
   display: flex;
   flex-wrap: wrap;
 }
@@ -83,7 +75,7 @@ main {
   height: 100%;
 }
 .flexWrap1 {
-  display: flex;
+  display: inline-flex;
   width: 50%;
   height: 50%;
 }
