14cbe64d5084a40d6e0cf5f4ed8221d5115b8c5f
HoonJae <rlagnswo0505@naver.com>
2022-08-05 12:10:09 +0900

    story 반응형 빼고 완성

--- frontend/src/views/story.vue
+++ frontend/src/views/story.vue
@@ -1,84 +1,86 @@
 <template>
   <div class="story-container" @wheel="wheelEvent">
     <div class="section column-center">
-      <img src="@/assets/img/story/visual-logo.png" alt="" />
-      <h3>내 피부 본연의 건강한 빛, 내 삶의 빛나는 자신감.</h3>
-      <h4>Radiance on my skin, Confidence in my life.</h4>
+      <img data-aos="fade-up" data-aos-duration="1000" src="@/assets/img/story/visual-logo.png" alt="" />
+      <h3 data-aos="fade-up" data-aos-duration="2000">내 피부 본연의 건강한 빛, 내 삶의 빛나는 자신감.</h3>
+      <h4 data-aos="fade-up" data-aos-duration="2000">Radiance on my skin, Confidence in my life.</h4>
       <div class="down-icon"><i class="fa-solid fa-angle-down"></i></div>
     </div>
     <div class="section column-center ms-5 me-5">
       <div class="flex-center">
-        <img src="@/assets/img/story/glow01.png" class="mw-40 me-5" alt="" />
+        <img data-aos="fade-up" data-aos-duration="1000" src="@/assets/img/story/glow01.png" class="mw-40 me-5" alt="" />
         <div class="ms-5 me-5 mw-40">
-          <h2>라네즈는 피부의 건강한 빛은 물론<br />삶까지 빛나는 자신감을 선사합니다.</h2>
-          <p>
-            라네즈는 누군가 정의한 아름다움을 따르는 것이 아닌 개개인의<br />
-            다양성과 자신만의 반짝임을 존중합니다.
-          </p>
-          <p>
-            자신의 피부가 가진 건강함을 되찾을 수 있도록 라네즈는 오늘도<br />
-            당신의 피부를 위한 최적의 솔루션을 찾기 위해 분석하고 연구하죠.
-          </p>
-          <p>
-            라네즈가 선사하는 최적의 피부 솔루션으로 피부 본연의 건강한 빛을 되찾고<br />
-            더 나아가 스스로를 믿고 자신감을 가지고 나아가세요.
-          </p>
-          <p>라네즈와 함께라면 언제나 빛나고 아름다울 거에요.</p>
-          <h3>FEEL the GLOW with LANEIGE.</h3>
+          <h2 data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">라네즈는 피부의 건강한 빛은 물론<br />삶까지 빛나는 자신감을 선사합니다.</h2>
+          <div class="txt-box" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1000">
+            <p>
+              라네즈는 누군가 정의한 아름다움을 따르는 것이 아닌 개개인의<br />
+              다양성과 자신만의 반짝임을 존중합니다.
+            </p>
+            <p>
+              자신의 피부가 가진 건강함을 되찾을 수 있도록 라네즈는 오늘도<br />
+              당신의 피부를 위한 최적의 솔루션을 찾기 위해 분석하고 연구하죠.
+            </p>
+            <p>
+              라네즈가 선사하는 최적의 피부 솔루션으로 피부 본연의 건강한 빛을 되찾고<br />
+              더 나아가 스스로를 믿고 자신감을 가지고 나아가세요.
+            </p>
+            <p>라네즈와 함께라면 언제나 빛나고 아름다울 거에요.</p>
+            <h3>FEEL the GLOW with LANEIGE.</h3>
+          </div>
         </div>
       </div>
     </div>
     <div class="section column-center">
       <div class="column-center">
-        <img src="@/assets/img/story/glow_logo.png" class="mb-3" alt="" />
-        <h1 class="mb-5">BEAUTY & LIFE LAB</h1>
+        <img data-aos="fade-up" data-aos-duration="1000" src="@/assets/img/story/glow_logo.png" class="mb-3" alt="" />
+        <h1 class="mb-5" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">BEAUTY & LIFE LAB</h1>
         <div class="flex-center mt-2">
           <div class="mw-40 txt">
-            <i class="fa-solid fa-quote-left"></i>
-            <h2>
+            <i data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600" class="fa-solid fa-quote-left"></i>
+            <h2 data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
               라네즈 Beauty & Life Lab의 연구는 <br />
               고객의 24/7 라이프스타일에서 시작합니다.
             </h2>
-            <p>
+            <p data-aos="fade-up" data-aos-duration="1000" data-aos-delay="900">
               오랜 시간 이어온 피부 속 보습 메커니즘 연구에 더해<br />
               피부 밖의 일상을 관찰하며 피부 안과 밖을 모두 아우르는<br />
               똑똑한 처방이 담긴 제품을 만듭니다.
             </p>
           </div>
-          <img src="@/assets/img/story/glow02.png" class="mw-40 ms-5 me-5" alt="" />
+          <img data-aos="fade-up" data-aos-duration="1000" data-aos-delay="900" src="@/assets/img/story/glow02.png" class="mw-40 ms-5 me-5" alt="" />
         </div>
       </div>
     </div>
     <div class="last-section">
       <div class="column-center">
-        <img src="@/assets/img/story/glow_logo.png" class="mb-3 glow-logo" alt="" />
-        <h1 class="mb-5">MAIN PRODUCT</h1>
-        <p class="last-subtitle">
+        <img data-aos="fade-up" data-aos-duration="1000" src="@/assets/img/story/glow_logo.png" class="mb-3 glow-logo" alt="" />
+        <h1 data-aos="fade-up" data-aos-duration="1000" class="mb-5">MAIN PRODUCT</h1>
+        <p class="last-subtitle" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
           피부 속 보습 메커니즘에 기반한 오랜 연구와 라이프스타일 연구를 더해 완성된<br />
           라네즈의 5가지 기능성 솔루션
         </p>
-        <p class="vertical-line"></p>
+        <p class="vertical-line" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300"></p>
         <div class="main-products">
           <div class="main-product mt-2">
             <div class="product-wrap bg-blue-color">
               <div class="txt-wrap odd">
-                <div class="number-title d-flex blue-color">
+                <div class="number-title d-flex blue-color" data-aos="fade-up" data-aos-duration="1000">
                   <h5 class="num">01</h5>
                   <h3>발라도 발라도<br />피부 갈증이 해결되지 않는다면?</h3>
                 </div>
-                <div class="product-name blue-color">
+                <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" class="product-name blue-color">
                   <p class="performance">FULL REPAIR MOISTURIZING</p>
                   <p class="name">워터뱅크 블루 히알루로닉</p>
                 </div>
-                <div class="product-txt">
+                <div class="product-txt" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
                   <p>
-                    매일 반복되는 피부 스트레스, 블루라이트, 수면부족...&nbsp;<br />
+                    매일 반복되는 피부 스트레스, 블루라이트, 수면부족...<br />
                     요즘 건조에 필요한 건 수분만이 아니죠.
                   </p>
                   <p>
                     새로워진 워터뱅크는 38년 연구 끝에 개발된 라네즈 독자 성분<br />
                     블루 히알루론산을 탑재하여 300%<sub style="bottom: 0.5em">*</sub> 더 강력한 장벽 개선 효과를 선사합니다.<br />
-                    수분에 더해 장벽까지 잡아주는&nbsp;Full 리페어 보습의 힘을 느껴보세요.
+                    수분에 더해 장벽까지 잡아주는 Full 리페어 보습의 힘을 느껴보세요.
                   </p>
                   <p>
                     *블루 히알루론산 원료 특성에 한함(일반 고분자 히알루론산 대비<br />
@@ -87,7 +89,7 @@
                 </div>
               </div>
 
-              <div class="product-img">
+              <div class="product-img" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
                 <img src="https://www.laneige.com/kr/ko/assets/image/brand/feel-the-glow/laneige__brand_story_pc_220317.jpg" alt="" />
               </div>
             </div>
@@ -95,18 +97,18 @@
           <div class="main-product even mt-2">
             <div class="product-wrap even bg-brown-color">
               <div class="txt-wrap">
-                <div class="number-title d-flex brown-color">
+                <div class="number-title d-flex brown-color" data-aos="fade-up" data-aos-duration="1000">
                   <h5 class="num">02</h5>
                   <h3>
                     칙칙해진 피부 톤이<br />
                     걱정이라면?
                   </h3>
                 </div>
-                <div class="product-name brown-color">
+                <div class="product-name brown-color" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
                   <p class="performance">BRIGHTENING CARE</p>
                   <p class="name">래디언-C 라인</p>
                 </div>
-                <div class="product-txt">
+                <div class="product-txt" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
                   <p>
                     이유를 모르게 피부가 자꾸 칙칙해지는 원인은 무엇일까요? <br />
                     햇빛의 자외선은 물론 우리의 일상에 깊숙이 자리잡은 스마트폰과 <br class="pc-only" />
@@ -119,22 +121,22 @@
                 </div>
               </div>
               <div class="product-img">
-                <img src="https://www.laneige.com/kr/ko/assets/image/brand/feel-the-glow/glow04-img02-pc.jpg" alt="" />
+                <img data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600" src="https://www.laneige.com/kr/ko/assets/image/brand/feel-the-glow/glow04-img02-pc.jpg" alt="" />
               </div>
             </div>
           </div>
           <div class="main-product mt-2">
             <div class="product-wrap bg-blue-color">
               <div class="txt-wrap odd">
-                <div class="number-title d-flex purple-color">
+                <div class="number-title d-flex purple-color" data-aos="fade-up" data-aos-duration="1000">
                   <h5 class="num">03</h5>
                   <h3>점점 더 커지는<br />모공과 주름이 고민이라면?</h3>
                 </div>
-                <div class="product-name purple-color">
+                <div class="product-name purple-color" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
                   <p class="performance">ANTI-AGING CARE</p>
                   <p class="name">퍼펙트 리뉴 유스 라인</p>
                 </div>
-                <div class="product-txt">
+                <div class="product-txt" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
                   <p>
                     어느 날 갑자기 눈에 띄는 세로 모공이 생기고<br />
                     뺨 피부가 나도 모르게 부쩍 아래로 처진 느낌이 드시나요?<br />
@@ -149,7 +151,7 @@
                   </p>
                 </div>
               </div>
-              <div class="product-img">
+              <div class="product-img" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
                 <img src="https://www.laneige.com/kr/ko/assets/image/brand/feel-the-glow/glow04-img03-pc.jpg" alt="" />
               </div>
             </div>
@@ -157,15 +159,15 @@
           <div class="main-product even mt-2">
             <div class="product-wrap even bg-dark-blue-color">
               <div class="txt-wrap">
-                <div class="number-title d-flex dark-blue-color">
+                <div class="number-title d-flex dark-blue-color" data-aos="fade-up" data-aos-duration="1000">
                   <h5 class="num">04</h5>
                   <h3>잠은 못 자도 피부는<br />좋아 보일 순 없을까?</h3>
                 </div>
-                <div class="product-name dark-blue-color">
+                <div class="product-name dark-blue-color" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
                   <p class="performance">SLEEPING CARE</p>
                   <p class="name">슬리핑 라인</p>
                 </div>
-                <div class="product-txt">
+                <div class="product-txt" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
                   <p>수면 시간이 부족하면 건강에는 물론, 피부에도 불균형 상태를 초래합니다.</p>
                   <p>
                     피부 수분 손실이 증가하고, 피지 분비가 감소되는<br />
@@ -176,7 +178,7 @@
                   <p>‘워터 슬리핑 마스크 EX’로 푹 잔 듯 맑고 투명한 아침 피부를 만들어 보세요.</p>
                 </div>
               </div>
-              <div class="product-img">
+              <div class="product-img" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
                 <img src="https://www.laneige.com/kr/ko/assets/image/brand/feel-the-glow/glow04-img04-pc.jpg" alt="" />
               </div>
             </div>
@@ -184,15 +186,15 @@
           <div class="main-product mt-2">
             <div class="product-wrap bg-green-color">
               <div class="txt-wrap odd">
-                <div class="number-title d-flex green-color">
+                <div class="number-title d-flex green-color" data-aos="fade-up" data-aos-duration="1000">
                   <h5 class="num">05</h5>
                   <h3>내 피부처럼 감쪽같이<br />커버될 수 없을까?</h3>
                 </div>
-                <div class="product-name green-color">
+                <div class="product-name green-color" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">
                   <p class="performance">BASE MAKEUP</p>
                   <p class="name">네오 쿠션</p>
                 </div>
-                <div class="product-txt">
+                <div class="product-txt" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
                   <p>
                     액티브한 야외 활동에도 들뜸없이,<br />
                     핸드폰 액정, 마스크에도 묻어남 없는 베이스 메이크업이 필요하신가요?
@@ -208,7 +210,7 @@
                   </p>
                 </div>
               </div>
-              <div class="product-img">
+              <div class="product-img" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
                 <img src="https://www.laneige.com/kr/ko/assets/image/brand/feel-the-glow/glow04-img05-pc.jpg" alt="" />
               </div>
             </div>
@@ -217,18 +219,18 @@
       </div>
     </div>
     <div class="life-style column-center">
-      <h1>LANEIGE LIFE STYLE SCENE</h1>
-      <h4>#LIVE MY OWN LIFE</h4>
+      <h1 data-aos="fade-up" data-aos-duration="1000">LANEIGE LIFE STYLE SCENE</h1>
+      <h4 data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">#LIVE MY OWN LIFE</h4>
       <div class="video-box">
         <video autoplay preload muted loop src="https://www.laneige.com/kr/ko/assets/image/brand/feel-the-glow/glow-video.mp4"></video>
       </div>
     </div>
     <div class="story-footer column-center">
-      <h3>
+      <h3 data-aos="fade-up" data-aos-duration="1000">
         라네스프리가 선사하는 새로운 뷰티 경험을 통해<br />
         당신의 삶이 더 즐겁고 자신감으로 빛나길 응원합니다
       </h3>
-      <div class="white-logo"><img src="@/assets/img/story/glow-logo-white.png" alt="" /></div>
+      <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" class="white-logo"><img src="@/assets/img/story/glow-logo-white.png" alt="" /></div>
     </div>
   </div>
 </template>
@@ -238,7 +240,9 @@ export default {
   data() {
     return {};
   },
-
+  mounted() {
+    AOS.init();
+  },
   methods: {
     wheelEvent() {
       const elm = document.querySelectorAll('.section');
@@ -319,6 +323,12 @@ export default {
 .section h4 {
   font-size: 18px;
   font-weight: normal;
+}
+.section:first-child h3 {
+  font-size: 24px;
+}
+.section:first-child h4 {
+  font-size: 20px;
 }
 .section p {
   margin: 20px 0;
