5388ee783bb7db7b1f27e0d7ae9c030940a38173
Seung-jae <tmdwo5040@gmail.com>
2022-08-06 10:39:16 +0900

    네비게이션 가드 next signin

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -17,6 +17,7 @@ import store from '@/store';
 const requireAuth = () => (to, from, next) => {
   if (store.state.setUser === 0) {
     alert('로그인 후에 이용해주시길 바랍니다.', '', 'warning');
+    next('/signin');
     return;
   }
   return next();
