d7512f9c456abf9d937c642671a9e33613b5e680
HoonJae <rlagnswo0505@naver.com>
2022-08-08 17:43:52 +0900

    chat 만들기

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,12 @@ import store from './store';
 import mixins from './mixins';
 import css from './assets/css/style.css';
 import swipe from '@/assets/js/swiper.js';
+import socketPlugin from './plugins/socketPlugin';
+
 // import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-createApp(App).use(store).use(router).mixin(mixins).use(css).use(swipe).mount('#app');
+createApp(App).use(store).use(router).mixin(mixins).use(css).use(swipe).use(socketPlugin).mount('#app');
 window.Kakao.init('61b4bcbd93b6e2039a3f7f7d46cbbe45');
 
 // new Vue({
