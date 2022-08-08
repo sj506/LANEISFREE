b1eade79ed052b43cf8b17463ffa6d72e1b13018
macadamia94 <thswlgus15@hanmail.net>
2022-08-08 18:15:00 +0900

    .

--- frontend/src/plugins/socketPlugin.js
+++ frontend/src/plugins/socketPlugin.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io.connect('http://192.168.0.52:3000', {
+const socket = io.connect('http://192.168.0.39:3000', {
   path: '/socket.io',
   transports: ['websocket'],
 });
