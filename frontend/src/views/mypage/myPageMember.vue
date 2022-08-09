9e250502a6b68068740c66969965af9e57176ce4
macadamia94 <thswlgus15@hanmail.net>
2022-08-10 03:04:50 +0900

    mypageMember 작업중

--- frontend/src/views/mypage/myPageMember.vue
+++ frontend/src/views/mypage/myPageMember.vue
@@ -21,14 +21,14 @@
     <section class="contents d-flex row align-items-baseline justify-content-between">
       <myPageSide />
       <div class="col-10 p-5 memberHeader">
-        <h3>뷰티포인트 통합 회원 정보 수정</h3>
+        <h3 class="pb-4">뷰티포인트 통합 회원 정보 수정</h3>
         <p class="mb-3 text-dark">* 이 페이지에서 회원정보 수정, 비밀번호 수정시 뷰티포인트 통합 아이디로 로그인하는 아모레퍼시픽 사이트의 정보가 함께 변경됩니다.</p>
         <div class="memberDiv">
           <div class="memberE" @click="displayE" :class="{ bgGreen: isActive1 }">회원정보 수정</div>
           <div class="memberP" @click="displayP" :class="{ bgGreen: isActive2 }">비밀번호 수정</div>
         </div>
-        <div class="memberE_ctnt" :class="{ dNone: isActive2}">
-          <h4>SNS 계정연동 관리</h4>
+        <div class="memberE_ctnt" :class="{ dNone: isActive2 }">
+          <h4 class="mt-4 pt-2 mb-4">SNS 계정연동 관리</h4>
           <table class="firstT">
             <thead>
               <tr>
@@ -61,53 +61,81 @@
             </tbody>
           </table>
           <div class="sub">이름 변경(개명)의 경우 이니스프리 고객상담실(080-380-0114)로 문의 부탁드립니다.</div>
-          <table class="seconT">
-            <tr>
-              <th>이름</th>
-              <td>쌀과자</td>
-            </tr>
-            <tr>
-              <th>이메일</th>
+          <div class="seconTdiv">
+            <table class="seconT">
+              <tr>
+                <th>이름</th>
+                <td>쌀과자</td>
+              </tr>
+              <tr>
+                <th>이메일</th>
+                <td>
+                  <input type="text" class="inputText"> @
+                  <input type="text" class="inputText" disabled>
+                  <select class="inputText ms-4">
+                    <option value="gmail.com">선택하세요</option>
+                    <option value="gmail.com">구글</option>
+                    <option value="naver.com">네이버</option>
+                    <option value="daum.net">다음</option>
+                    <option value="hanmail.net">한메일</option>
+                  </select>
+                  <span><input type="submit" class="noConnectBtn" value="메일확인"></span>
+                </td>
+              </tr>
+              <tr>
+                <th>휴대전화 번호</th>
+                <td>
+                  <input type="text" class="inputText" disabled> -
+                  <input type="text" class="inputText" disabled> -
+                  <input type="text" class="inputText" disabled>
+                  <input type="submit" class="noConnectBtn" value="변경">
+                </td>
+              </tr>
+              <tr>
+                <th>주소</th>
+                <td>
+                  <div>
+                    <input type="text" class="inputText" style="width: 13%;" disabled>
+                    <input type="submit" class="noConnectBtn" value="우편번호 검색">
+                  </div>
+                  <div>
+                    <input type="text" class="inputText mt-2" style="width: 60%;" disabled>
+                  </div>
+                  <div>
+                    <input type="text" class="inputText mt-2" style="width: 60%;">
+                  </div>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div class="sub2">
+            <h4>※ 개인정보 수집·이용 동의 (선택)</h4>
+            <p>회사는 이용자의 회원서비스 제공을 위하여 아래와 같이 개인정보를 수집 및 이용합니다.</p>
+          </div>
+          <table class="thirdT">
+            <thead>
+              <tr>
+                <th>수집항보</th>
+                <th>수집・이용목적</th>
+                <th>보유기간</th>
+              </tr>
+            </thead>
+            <tbody>
+              <td>이메일, 휴대전화 번호, 주소</td>
               <td>
-                <input type="text" class="inputText"> @
-                <input type="text" class="inputText" disabled>
-                <select>
-                  <option value="gmail.com" class="inputText">선택하세요</option>
-                  <option value="gmail.com">구글</option>
-                  <option value="naver.com">네이버</option>
-                  <option value="daum.net">다음</option>
-                  <option value="hanmail.net">한메일</option>
-                </select>
-                <span><input type="submit" class="noConnectBtn" value="메일확인"></span>
+                본인 동의 시 회사 또는 제휴사의<br>
+                서비스/사업 및 정책/기타 이벤트에 관한 정보 제공 및<br>
+                그에 따른 경품 등 물품 배송
               </td>
-            </tr>
-            <tr>
-              <th>휴대전화 번호</th>
-              <td>
-                <input type="text" class="inputText" disabled> -
-                <input type="text" class="inputText" disabled> -
-                <input type="text" class="inputText" disabled>
-                <input type="submit" value="변경">
-              </td>
-            </tr>
-            <tr>
-              <th>주소</th>
-              <td>
-                <div>
-                  <input type="text" class="inputText" style="width: 13%;" disabled>
-                  <input type="submit" value="우편번호 검색">
-                </div>
-                <div>
-                  <input type="text" class="inputText" style="width: 70%;" disabled>
-                </div>
-                <div>
-                  <input type="text" class="inputText" style="width: 70%;">
-                </div>
-              </td>
-            </tr>
+              <td>동의 철회 또는 회원 탈퇴 시 까지</td>
+            </tbody>
           </table>
+          <div>
+            <p>※ 귀하는 개인정보 수집 및 이용에 대한 동의를 거부할 수 있습니다. 단, 거부할 경우 문의 서비스의 이용이 불가합니다.</p>
+            <label><input type="checkbox">동의합니다.</label>
+          </div>
         </div>
-        <div class="memberP_ctnt" :class="{ dNone: isActive1}">
+        <div class="memberP_ctnt" :class="{ dNone: isActive1 }">
 
         </div>
       </div>
@@ -145,6 +173,8 @@ export default {
 <style scoped>
 
 
+
+
 p {
   color: #777;
   padding: 3px;
@@ -153,7 +183,7 @@ p {
 h3 {
   font-size: 30px;
   font-weight: 600;
-  padding-bottom: 20px;
+  /* padding-bottom: 20px; */
 }
 
 a {
@@ -169,13 +199,21 @@ table {
   table-layout: fixed;
 }
 
-th, td {
+th,
+td {
   padding: 17px 16px;
 }
 
 thead th {
   width: 170px;
   border-top: 2px solid #222;
+}
+
+.seconTdiv {
+  width: 1026px;
+  border-top: 1px solid #222;
+  border-bottom: 1px solid #dcdcdc;
+  padding: 15px 0;
 }
 
 .seconT th {
@@ -195,12 +233,21 @@ thead th {
   vertical-align: middle;
 }
 
-.memberE_ctnt h4 {
-  margin: 35px 0 24px 0;
+.col {
+  width: 35px;
 }
 
-.col {
-  width: 50px;
+.thirdT thead th,
+.thirdT tbody td {
+  border-bottom: 1px solid #dcdcdc;
+  border-left: 1px solid #dcdcdc;
+  text-align: center;
+  padding: 17px 14px;
+}
+
+.thirdT th:first-child,
+.thirdT td:first-child {
+  border-left: none;
 }
 
 .noConnect {
@@ -213,13 +260,17 @@ thead th {
   color: #222;
   font-size: 14px;
   margin: 0 6px;
-  width: 82px;
-  height: 30px;
+  padding: 0 16px;
+  height: 40px;
 }
 
 .sub {
   margin: 75px 0 24px;
   color: #777;
+}
+
+.sub2 {
+  margin: 75px 0 24px;
 }
 
 .inputText {
@@ -348,6 +399,4 @@ thead th {
   justify-content: center;
   align-items: center;
 }
-
-
 </style>
