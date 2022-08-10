7d9b5f454240f3ce16c7e47ee6c0f1f2ad5df530
HoonJae <rlagnswo0505@naver.com>
2022-08-10 09:55:14 +0900

    오류 찾는 중

--- frontend/src/components/ChatView.vue
+++ frontend/src/components/ChatView.vue
@@ -79,4 +79,131 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.chat-container.dNone {
+  display: none;
+}
+.chat-container {
+  position: fixed;
+  z-index: 3;
+  bottom: 30px;
+  right: 30px;
+  width: 360px;
+  max-height: 640px;
+  box-shadow: var(--box-shadow);
+  background-color: #a9e8fe;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 20px;
+}
+
+.chat-header {
+  position: relative;
+  background-color: var(--bg-main);
+  color: var(--text-white);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 48px;
+}
+.chat-section {
+  align-items: flex-end;
+}
+.chat-header .chat-title {
+  padding: 0;
+  margin: 0;
+}
+.chat-box {
+  margin: 5px 0;
+}
+.btnClose {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 20px;
+  background: inherit;
+  border: none;
+  box-shadow: none;
+  border-radius: 0;
+  padding: 0;
+  overflow: visible;
+  cursor: pointer;
+  font-size: 24px;
+  color: var(--text-white);
+}
+.chat-body {
+  width: 100%;
+  height: 300px;
+  overflow-y: auto;
+  padding: 0 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+}
+.user-name {
+  font-size: 10px;
+  color: var(--text-dark-gray);
+}
+.chat-footer {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+}
+.chat-footer .input-box {
+  position: relative;
+}
+.chat-footer input[type='text'] {
+  width: 300px;
+  padding: 10px;
+  padding-right: 60px;
+  border-radius: 10px;
+  background-color: var(--bg-white);
+  border: none;
+}
+.btnSend {
+  position: absolute;
+  background-color: var(--bg-white);
+  padding: 10px;
+  right: 5px;
+  /* z-index: 1; */
+  border: none;
+  color: var(--text-main);
+  font-size: 18px;
+}
+
+.message {
+  margin-top: 4px;
+  padding: 10px;
+  background-color: var(--bg-white);
+  border-radius: 0 5px 5px 5px;
+}
+.message.sent {
+  background-color: var(--bg-main);
+  color: var(--text-white);
+
+  border-radius: 5px 0px 5px 5px;
+}
+.sent {
+  flex-direction: row-reverse;
+}
+.circle-img {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.circle-img img {
+  width: 100%;
+}
+.chat-footer input[type='text']:focus {
+  outline: none;
+}
+.time {
+  font-size: 12px;
+  color: var(--text-gray);
+  margin: 5px;
+}
+</style>
