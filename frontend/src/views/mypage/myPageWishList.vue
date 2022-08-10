142880c2f195ba4c5f8bed9e484c331b25d62060
Seung-jae <tmdwo5040@gmail.com>
2022-08-10 11:18:25 +0900

    mypagesidebar

--- frontend/src/views/mypage/myPageWishList.vue
+++ frontend/src/views/mypage/myPageWishList.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="container">
+    <div class="location headerCate">
+      <div class="header">
+        <router-link to="/">
+          <div class="depth"><button href="/" class="btn">홈</button></div>
+        </router-link>
+
+        <div class="depth">
+          <button type="button" class="btn">마이페이지</button>
+        </div>
+
+        <div class="depth">
+          <button type="button" class="btn">찜한 제품</button>
+        </div>
+      </div>
+    </div>
     <myPageHeader />
     <section class="contents d-flex row align-items-baseline justify-content-between">
-      <myPageSide />
-      <div class="col-9 reviewHeader">
+      <myPageSide activeLink="/myPageWishList" />
+      <div class="col-10 p-5">
         <h3>찜한 제품</h3>
         <!-- <div class="reviewDiv">
           <div class="reviewP" @click="displayP" :class="{ bgGreen: isActive1 }">작성 가능한 리뷰</div>
@@ -271,4 +286,55 @@ td {
   border: none;
   color: var(--text-white);
 }
+
+/* header category */
+
+.location {
+  display: flex;
+  flex-direction: column;
+}
+
+.location:after {
+  content: '';
+  display: block;
+  clear: both;
+}
+
+.header {
+  margin-left: 80px;
+}
+
+.headerCate {
+  max-width: 1550px;
+  margin: auto;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+}
+
+.depth {
+  float: left;
+  margin-left: 10px;
+  padding: 15px 0 17px;
+  background: url('@/assets/img/mypage/arw_loc.png') no-repeat 0 50%;
+}
+
+.depth:first-child {
+  background: none;
+}
+
+.btn {
+  margin: 0 0 0 15px;
+  padding-right: 25px;
+  color: #777;
+  background: url('@/assets/img/mypage/arw_loc_btn.png') no-repeat 99% 50%;
+}
+
+.depth:last-child .btn {
+  color: #222;
+}
+
+.depth:first-child .btn {
+  color: #777;
+}
+/* header category */
 </style>
